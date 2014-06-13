36babfc6ba3d74bec4f0f6586a396d7fb9739735
Sami Kerola <kerolasa@iki.fi>
2014-06-13 20:34:07 +0100

    make list lenght dynamic
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- outlier.c
+++ outlier.c
@@ -1,6 +1,25 @@
+#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+static void *xmalloc(const size_t size)
+{
+	void *ret = malloc(size);
+
+	if (!ret && size)
+		err(EXIT_FAILURE, "cannot allocate %zu bytes", size);
+	return ret;
+}
+
+static void *xrealloc(void *ptr, const size_t size)
+{
+	void *ret = realloc(ptr, size);
+
+	if (!ret && size)
+		err(EXIT_FAILURE, "cannot allocate %zu bytes", size);
+	return ret;
+}
 
 static int __attribute__((__pure__)) comp_double(const void *a, const void *b)
 {
@@ -12,14 +31,25 @@ static int __attribute__((__pure__)) comp_double(const void *a, const void *b)
 
 int main(void)
 {
-	double list[32768], d, mean, q1, q3, range;
-	int n = 0, matches;
+	double *list, *lp, d, mean, q1, q3, range;
+	int matches;
+	size_t n = 0, list_sz = 0x8000;
+
+	list = xmalloc(list_sz * sizeof(double));
+	lp = list;
 	while (!feof(stdin)) {
 		matches = scanf("%le", &d);
 		if (matches == 0 || isnan(d))
 			continue;
-		list[n] = d;
+		*lp = d;
 		n++;
+		if (list_sz < n) {
+			list_sz *= 2;
+			list = xrealloc(list, (list_sz * sizeof(double)));
+			lp = list;
+			lp += n - 1;
+		}
+		lp++;
 	}
 	qsort(list, n, sizeof(double), &comp_double);
 	q1 = list[n / 4];
@@ -28,5 +58,6 @@ int main(void)
 	range = q3 - q1;
 	printf("lof: %f q1: %f m: %f q3: %f hof: %f (range: %f)\n", q1 - range,
 	       q1, mean, q3, q3 + range, range);
+	free(list);
 	return 0;
 }
