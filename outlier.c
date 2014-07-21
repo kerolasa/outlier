6cb4ecd26d785b65a3e6f318f44f1b08f4e30129
Sami Kerola <kerolasa@iki.fi>
2014-07-21 12:17:53 +0100

    check status of output before exiting
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- outlier.c
+++ outlier.c
@@ -50,6 +50,10 @@
 #include <string.h>
 #include <unistd.h>
 
+#ifdef HAVE_STDIO_EXT_H
+# include <stdio_ext.h>
+#endif
+
 #define DEFAULT_MULTIPLIER 1.5
 #define RRD_DATA "/rrd/rra/database/row/v"
 #define RRD_MIN "/rrd/ds/min"
@@ -85,6 +89,40 @@ static void __attribute__((__noreturn__)) usage(FILE *out)
 	fputs("\n", out);
 	fprintf(out, "For more details see %s(1).\n", PACKAGE_NAME);
 	exit(out == stderr ? EXIT_FAILURE : EXIT_SUCCESS);
+}
+
+#ifndef HAVE___FPENDING
+static inline int __fpending(FILE *stream __attribute__((__unused__)))
+{
+	return 0;
+}
+#endif
+
+static inline int close_stream(FILE * stream)
+{
+	const int some_pending = (__fpending(stream) != 0);
+	const int prev_fail = (ferror(stream) != 0);
+	const int fclose_fail = (fclose(stream) != 0);
+
+	if (prev_fail || (fclose_fail && (some_pending || errno != EBADF))) {
+		if (!fclose_fail && !(errno == EPIPE))
+			errno = 0;
+		return EOF;
+	}
+	return 0;
+}
+
+static inline void close_stdout(void)
+{
+	if (close_stream(stdout) != 0 && !(errno == EPIPE)) {
+		if (errno)
+			warn("write error");
+		else
+			warnx("write error");
+		_exit(EXIT_FAILURE);
+	}
+	if (close_stream(stderr) != 0)
+		_exit(EXIT_FAILURE);
 }
 
 static void *xmalloc(const size_t size)
@@ -315,6 +353,7 @@ int main(int argc, char **argv)
 		{"help", no_argument, NULL, 'h'},
 		{NULL, 0, NULL, 0}
 	};
+	atexit(close_stdout);
 	memset(&conf, 0, sizeof(conf));
 	conf.whiskers = DEFAULT_MULTIPLIER;
 	conf.list_sz = 0x8000;
